0ba301b48efa413e8c40fa2c204c9b3d048f1362
surya-neerajtata <neeraj.tata@kcl.ac.uk>
2018-07-08 22:56:42 +0100

    Create DeviceControl.cpp

---
+++ Cpp/DeviceControl.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <APTAPI.h>
+
+int main()
+{
+  std::cout << "Hello World!";
+  MOT_GetPosition(0,0); // Put random function from header file, 'identify' is the best.
+}
